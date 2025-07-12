45b3a7486cb6bc44fb4d0348732ac48835e0320d
jensen-westraad <jensenwestraad@gmail.com>
2025-07-12 15:46:38 +0200

    complete step 3 and step 4

---
+++ scripts/temples.js
@@ -0,0 +1,6 @@
+document.getElementById("menu-toggle").addEventListener("click", () => {
+    document.getElementById("main-nav").classList.toggle("hide");
+});
+
+
+document.getElementById("lastModified").textContent = document.lastModified;
