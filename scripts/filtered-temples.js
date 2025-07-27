cc7eab4f26f7cf65eb4e462f4fdc077dc58d5501
jensen-westraad <jensenwestraad@gmail.com>
2025-07-27 11:44:10 +0200

    created all the needed files

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,25 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const footer = document.querySelector("footer");
+    const year = new Date().getFullYear();
+    const copyrightText = `©${year} 🎞️ Jensen Anthony Westraad 🎞️ South Africa`;
+    footer.querySelector("p").innerHTML = copyrightText;
+
+
+    const lastModified = document.getElementById("lastModified");
+    if (lastModified) {
+        lastModified.textContent = document.lastModified;
+    }
+
+
+    const menuButton = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("main-nav");
+
+    menuButton.addEventListener("click", () => {
+        navMenu.classList.toggle("hide");
+
+        // Toggle icon between ☰ and ✖
+        menuButton.textContent = navMenu.classList.contains("hide") ? "☰" : "✖";
+    });
+});
